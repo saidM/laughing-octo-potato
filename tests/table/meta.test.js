778dd778343425c8624617d0f70bdacdcb815ca3
Saïd Mimouni <sayid.mimouni@gmail.com>
2021-07-11 11:24:51 +0200

    Add nullable property to table columns

--- tests/table/meta.test.js
+++ tests/table/meta.test.js
@@ -31,8 +31,8 @@ describe('GET /tables/:name/:meta', () => {
         .expect(200)
         .then(res => {
           expect(res.body.columns).toEqual([
-            { name: 'key', type: 'text' },
-            { name: 'value', type: 'text' }
+            { name: 'key', type: 'text', nullable: false },
+            { name: 'value', type: 'text', nullable: false }
           ]);
         });
     });
