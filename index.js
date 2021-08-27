4ae1d7a38e7afdb7980cadffa217a17754b38711
Saïd Mimouni <sayid.mimouni@gmail.com>
2021-08-27 11:45:36 +0200

    Use dynamic port

--- index.js
+++ index.js
@@ -62,9 +62,7 @@ app.use((err, req, res, next) => {
 });
 
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(3001, () => {
-    console.log('Server listening on port 3001');
-  });
+  app.listen(process.env.PORT || 3001);
 }
 
 module.exports = app;
