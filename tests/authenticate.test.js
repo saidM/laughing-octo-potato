4c2b9e925ff1122c5d062a8bb20c310f9ebc826a
Saïd Mimouni <sayid.mimouni@gmail.com>
2021-08-28 16:11:08 +0200

    Add tests for authenticate

---
+++ tests/authenticate.test.js
@@ -0,0 +1,28 @@
+require('./test_helper');
+
+describe('Authenticate Middleware', () => {
+  describe('when no token', () => {
+    it('returns 401', () => {
+      return request(app)
+      .get('/tables')
+      // .set('Authorization', 'Bearer azerty')
+      .expect(401);
+    });
+  });
+  describe('when invalid token', () => {
+    it('returns 401', () => {
+      return request(app)
+      .get('/tables')
+      .set('Authorization', 'Bearer invalid')
+      .expect(401);
+    });
+  });
+  describe('when token', () => {
+    it('returns 200', () => {
+      return request(app)
+      .get('/tables')
+      .set('Authorization', 'Bearer azerty')
+      .expect(200);
+    });
+  });
+});
