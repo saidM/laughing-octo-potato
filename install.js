c2b4cc95be8604c1205b6a75452418895599de7a
Saïd Mimouni <sayid.mimouni@gmail.com>
2021-08-28 16:30:40 +0200

    Set token with heroku command

--- install.js
+++ install.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { execSync } = require('child_process');
 
 const [, , token] = process.argv;
 if (typeof token === 'undefined') {
@@ -9,5 +10,6 @@ const content = `BRIDGE_TOKEN=${token}`;
 
 fs.writeFile('.env', content, (err) => {
   if (err) throw err;
+  execSync(`heroku config:set ${content}`);
   console.log('Installation completed! You can start your bridge by running: npm start adminplus');
 });
